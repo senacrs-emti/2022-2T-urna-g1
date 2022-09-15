a073100387e02dd33bde64916fd9b17a4bc47bc6
TayrelPreci <tayrelpreci@hotmail.com>
2022-09-15 16:47:47 -0300

    commit initial
    commit 0.1

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/style.css">
+
+</head>
+<body>
+    
+    <div class="block1">
+        
+        <div class="numeros">
+
+            <button type="submit" class="btn btn-block btn-primary num">1</button>
+            <button type="submit" class="btn btn-block btn-primary num">2</button>
+            <button type="submit" class="btn btn-block btn-primary num">3</button>
+            <button type="submit" class="btn btn-block btn-primary num">4</button>
+            <button type="submit" class="btn btn-block btn-primary num">5</button>
+            <button type="submit" class="btn btn-block btn-primary num">6</button>
+            <button type="submit" class="btn btn-block btn-primary num">7</button>
+            <button type="submit" class="btn btn-block btn-primary num">8</button>
+            <button type="submit" class="btn btn-block btn-primary num">9</button>
+            <button type="submit" style="margin-left: 96px;" class="btn btn-block btn-primary num">0</button>
+
+        </div>
+
+        <button type="submit" class="butn" >branco</button>
+
+        <button type="submit" class="butn" >corrige</button>
+        
+        <button type="submit" class="butn" style="height: 100px;" >confirma</button>
+
+    </div>
+
+
+    <script src="assets/script.js"></script>
+</body>
+</html>
