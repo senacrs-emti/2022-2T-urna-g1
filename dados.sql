96b5e4ff8e00907931aafe8b3ef23cb9cf8d302e
Ethan <93671399+TayrelPreci@users.noreply.github.com>
2022-11-10 17:04:54 -0300

    commit 0.7
    commit to BD

---
+++ dados.sql
@@ -0,0 +1,51 @@
+/* CARGOS */
+
+
+
+/* PARTIDOS */
+
+INSERT INTO partidos (PartidosID, Nome, Sigla, NumLegenda, Excluido) VALUES (1, 'Partido Musical', 'PMU', '81','F');
+INSERT INTO partidos (PartidosID, Nome, Sigla, NumLegenda, Excluido) VALUES (2, 'Partido dos Esportes', 'PES', '82','F');
+INSERT INTO partidos (PartidosID, Nome, Sigla, NumLegenda, Excluido) VALUES (3, 'Partido das Profissões', 'PPR', '83','F');
+INSERT INTO partidos (PartidosID, Nome, Sigla, NumLegenda, Excluido) VALUES (4, 'Partido dos Animes', 'PAN', '84','F');
+INSERT INTO partidos (PartidosID, Nome, Sigla, NumLegenda, Excluido) VALUES (5, 'Partido dos Personagens Literário - Filmes e Séries', ' PPLFS', '85','F');
+INSERT INTO partidos (PartidosID, Nome, Sigla, NumLegenda, Excluido) VALUES (6, 'Partido dos Games', ' PGAM', '86','F');
+INSERT INTO partidos (PartidosID, Nome, Sigla, NumLegenda, Excluido) VALUES (7, 'Partido da Tecnologia da Informação', ' PTI', '87','F');
+
+/* CANDIDATOS */
+
+
+
+/* TIPO */
+
+INSERT INTO `tipo` (`TipoID`, `Nome`, `Excluido`) VALUES (NULL, 'Eleição Geral Federal', 'f'), (NULL, 'Eleição Municipal', 'f');
+
+/* ESTADO */
+
+INSERT INTO estados (EstadoID, Nome, Sigla, Excluido) VALUES (35, 'São Paulo', ' SP','F');
+INSERT INTO estados (EstadoID, Nome, Sigla, Excluido) VALUES (41, 'Paraná', ' PR','F');
+INSERT INTO estados (EstadoID, Nome, Sigla, Excluido) VALUES (42, 'Santa Catarina', ' SC','F');
+INSERT INTO estados (EstadoID, Nome, Sigla, Excluido) VALUES (43, 'Rio Grande do Sul', ' RS','F');
+INSERT INTO estados (EstadoID, Nome, Sigla, Excluido) VALUES (50, 'Mato Grosso do Sul', ' MS','F');
+INSERT INTO estados (EstadoID, Nome, Sigla, Excluido) VALUES (11, 'Rondônia', ' RO','F');
+INSERT INTO estados (EstadoID, Nome, Sigla, Excluido) VALUES (12, 'Acre', ' AC','F');
+INSERT INTO estados (EstadoID, Nome, Sigla, Excluido) VALUES (13, 'Amazonas', ' AM','F');
+INSERT INTO estados (EstadoID, Nome, Sigla, Excluido) VALUES (14, 'Roraima', ' RR','F');
+INSERT INTO estados (EstadoID, Nome, Sigla, Excluido) VALUES (15, 'Pará', ' PA','F');
+INSERT INTO estados (EstadoID, Nome, Sigla, Excluido) VALUES (16, 'Amapá', ' AP','F');
+INSERT INTO estados (EstadoID, Nome, Sigla, Excluido) VALUES (17, 'Tocantins', ' TO','F');
+INSERT INTO estados (EstadoID, Nome, Sigla, Excluido) VALUES (21, 'Maranhão', ' MA','F');
+INSERT INTO estados (EstadoID, Nome, Sigla, Excluido) VALUES (24, 'Rio Grande do Norte', ' RN','F');
+INSERT INTO estados (EstadoID, Nome, Sigla, Excluido) VALUES (25, 'Paraíba', ' PB','F');
+INSERT INTO estados (EstadoID, Nome, Sigla, Excluido) VALUES (26, 'Pernambuco', ' PE','F');
+INSERT INTO estados (EstadoID, Nome, Sigla, Excluido) VALUES (27, 'Alagoas', ' AL','F');
+INSERT INTO estados (EstadoID, Nome, Sigla, Excluido) VALUES (28, 'Sergipe', ' SE','F');
+INSERT INTO estados (EstadoID, Nome, Sigla, Excluido) VALUES (29, 'Bahia', ' BA','F');
+INSERT INTO estados (EstadoID, Nome, Sigla, Excluido) VALUES (31, 'Minas Gerais', ' MG','F');
+INSERT INTO estados (EstadoID, Nome, Sigla, Excluido) VALUES (33, 'Rio de Janeiro', ' RJ','F');
+INSERT INTO estados (EstadoID, Nome, Sigla, Excluido) VALUES (51, 'Mato Grosso', ' MT','F');
+INSERT INTO estados (EstadoID, Nome, Sigla, Excluido) VALUES (52, 'Goiás', ' GO','F');
+INSERT INTO estados (EstadoID, Nome, Sigla, Excluido) VALUES (53, 'Distrito Federal', ' DF','F');
+INSERT INTO estados (EstadoID, Nome, Sigla, Excluido) VALUES (22, 'Piauí', ' PI','F');
+INSERT INTO estados (EstadoID, Nome, Sigla, Excluido) VALUES (23, 'Ceará', ' CE','F');
+INSERT INTO estados (EstadoID, Nome, Sigla, Excluido) VALUES (32, 'Espírito Santo', ' ES','F');
